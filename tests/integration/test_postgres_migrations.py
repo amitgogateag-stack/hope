2505e7b95954786c5c486355977b636482854a81
amitgogateag-stack <amitgogate.ag@gmail.com>
2026-09-12 11:10:55 -0400

    test: register signal immutability migration

--- tests/integration/test_postgres_migrations.py
+++ tests/integration/test_postgres_migrations.py
@@ -48,6 +48,7 @@ def test_postgres_migrations_apply_and_are_idempotent() -> None:
             "025_experiment_invalidation_immutability.sql",
             "026_universe_metadata_immutability.sql",
             "027_audit_event_immutability.sql",
+            "028_signal_immutability.sql",
         ]
         assert second == []
         assert connection.execute(text("SELECT 1 FROM information_schema.tables WHERE table_name='experiments'")).scalar_one() == 1
